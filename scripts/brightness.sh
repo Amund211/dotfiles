73c90617e0e5c9b348d71b72a4e0fb1ad68b2c09
Amund Eggen Svandal <aesvandal@gmail.com>
2024-10-01 10:07:47 +0200

    chore: Update brightness script
    Use brightnessctl instead of /sys/class/...
    
    shfmt

--- scripts/brightness.sh
+++ scripts/brightness.sh
@@ -1,33 +1,34 @@
 #! /usr/bin/env bash
 
-# inc/dec the screen brightness exponentially, to appear
-# as a linear progression to our logatrithmic eyes
-
-steps=("1" "3" "6" "10" "16" "25" "45" "85" "150" "255")
-brightnessLoc=/sys/class/backlight/radeon_bl0/brightness
-currBright=$(cat $brightnessLoc)
-
-# Finding current brightness step
-for s in ${!steps[@]}; do
-	[[ "$currBright" = "${steps[${s}]}" ]] && currStep=${s}
-done
-
-amtSteps=${#steps[@]}
-maxStep=$(($amtSteps-1))
+set -eux
 
 # Change (1,2,-1,-2...) is taken as first parameter
 # otherwise increments by one as default
 change=${1:-1}
 
-nextStep=$(($currStep+$change))
+# inc/dec the screen brightness exponentially, to appear
+# as a linear progression to our logatrithmic eyes
+
+steps=("1" "139" "373" "1003" "2692" "7225" "19393")
+currBright="$(brightnessctl get)"
+
+currStep='0'
+# Finding current brightness step
+for s in ${!steps[@]}; do
+	[[ "$currBright" = "${steps[${s}]}" ]] && currStep=${s} && break
+done
+
+amtSteps=${#steps[@]}
+maxStep=$(($amtSteps - 1))
+
+nextStep=$(($currStep + $change))
 
 # Confining nextStep in set {0,...amtSteps-1}
-nextStep=$(($nextStep<0?0:$nextStep))
-nextStep=$(($nextStep>=$maxStep?$maxStep:$nextStep))
+nextStep=$(($nextStep < 0 ? 0 : $nextStep))
+nextStep=$(($nextStep >= $maxStep ? $maxStep : $nextStep))
 
 # No change in step
-(( $currStep == $nextStep )) && exit 0
+(($currStep == $nextStep)) && exit 0
 
 nextBright=${steps[$nextStep]}
-echo $nextBright | sudo tee $brightnessLoc
-
+brightnessctl set "$nextBright"
