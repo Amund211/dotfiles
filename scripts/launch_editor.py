095d8b1941e19efe251d7ae95cf0d13437ce810d
Amund Eggen Svandal <aesvandal@gmail.com>
2025-05-05 13:47:53 +0200

    launch_editor: Always open in last opened nvim instance
    To "focus" an nvim instance, simply close and re-open.
    
    Used to open in a random instance.

--- scripts/launch_editor.py
+++ scripts/launch_editor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import errno
 import os
 import sys
 from pathlib import Path
@@ -11,10 +12,14 @@ file_path = sys.argv[1].replace("$", r"\$")  # Handle $ in path
 line_number = sys.argv[2]
 column = sys.argv[3]
 
-uid = os.getuid()
+run_dir = Path(f"/run/user/{os.getuid()}")
+nvim_socket_paths = run_dir.glob("nvim*")
+try:
+    most_recent_socket_path = max(nvim_socket_paths, key=lambda p: p.stat().st_ctime)
+except ValueError:  # Max iterable argument is empty
+    print("No nvim socket found.", file=sys.stderr)
+    sys.exit(errno.ESRCH)  # No such process
 
-path = Path(f"/run/user/{uid}")
-paths = path.glob("nvim*")
-socket_path = next(paths)
-
-attach("socket", path=str(socket_path)).command(f"e +{line_number} {file_path}")
+attach("socket", path=str(most_recent_socket_path)).command(
+    f"e +{line_number} {file_path}"
+)
